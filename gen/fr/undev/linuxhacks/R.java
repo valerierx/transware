313bb71ca4e562a203024ad2a215d0191d3cfeb9
undevdecatos <jeigfovhjnpien fpiokvjnebveniv>
2020-06-23 00:30:26 +0200

    RPC

---
+++ gen/fr/undev/linuxhacks/R.java
@@ -0,0 +1,10 @@
+package fr.undev.linuxhacks;
+
+import com.onkiup.minedroid.gui.resources.*;
+import com.onkiup.minedroid.gui.MineDroid;
+public final class R {
+	final static String MODID = "linuxware";
+	public final static class id {
+	}
+
+}
---
+++ src/main/java/fr/undev/linuxhacks/gui/Gui.java
@@ -0,0 +1,45 @@
+package fr.undev.linuxhacks.gui;
+
+import com.onkiup.minedroid.R;
+import com.onkiup.minedroid.Context;
+import com.onkiup.minedroid.gui.Overlay;
+import com.onkiup.minedroid.gui.events.MouseEvent;
+import com.onkiup.minedroid.gui.views.View;
+import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+
+@SideOnly(Side.CLIENT)
+public class Gui extends Overlay {
+
+    private View.OnClick onDismissClick = new View.OnClick() {
+        @Override
+        public void handle(MouseEvent event) {
+            dismiss();
+        }
+    };
+
+    public Gui(Context context) {
+        super(context);
+    }
+
+    @Override
+    protected ResourceLocation getContentLayout() {
+        return R.layout.minedroid_test;
+    }
+
+    @Override
+    protected void fill(View content) {
+    }
+
+    @Override
+    protected void onStart() {
+
+    }
+
+    @Override
+    protected void onStop() {
+
+    }
+
+}
