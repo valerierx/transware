82657c44987e5a845f2898b0272228a474191dc6
unknown <mikkmerzin1@gmail.com>
2020-07-02 20:10:27 +0300

    forgot the token logger so added it

--- src/main/java/fr/undev/transware/huds/HUDWelcome.java
+++ src/main/java/fr/undev/transware/huds/HUDWelcome.java
@@ -20,7 +20,7 @@ public class HUDWelcome extends HUD {
 
 	@Override
 	public void render() {
-		String huh = "hii :^) welcome " + Minecraft.getMinecraft().getSession().getUsername();
+		String huh = "hii :^) welcome uwu" + Minecraft.getMinecraft().getSession().getUsername(); //uwufy
 
 		create(huh, 10, 10, 255, 255, 255);
 	}
