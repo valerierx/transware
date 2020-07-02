7fad121cd04053d91bb7d238ad5d68e5157316d3
undevdecatos <jeigfovhjnpien fpiokvjnebveniv>
2020-07-02 19:40:20 +0200

    Packet event

---
+++ gen/fr/undev/transware/R.java
@@ -0,0 +1,14 @@
+package fr.undev.transware;
+
+import com.onkiup.minedroid.gui.resources.*;
+import com.onkiup.minedroid.gui.MineDroid;
+public final class R {
+	final static String MODID = "transware";
+	public final static class id {
+	}
+
+	public final static class string {
+		public final static ValueLink armorWarn = new ValueLink(new EnvValue[] { new EnvValue(null, null, null, null, "Warning! Armor under 50%") });
+	}
+
+}
---
+++ src/main/java/fr/undev/transware/event/PacketRecieved.java
@@ -0,0 +1,27 @@
+package fr.undev.transware.event;
+
+import net.minecraft.network.Packet;
+import net.minecraftforge.fml.common.eventhandler.Event;
+
+/**
+ * Created by linux!!
+ *
+ * @since 02/07/2020
+ **/
+
+public class PacketRecieved extends Event {
+    private Packet<?> packet;
+
+    public PacketRecieved(Packet<?> packet) {
+        this.packet = packet;
+    }
+
+    public final Packet<?> getPacket() {
+        return this.packet;
+    }
+
+
+    public final Packet<?> setPacket(Packet<?> packet) {
+        return this.packet = packet;
+    }
+}
