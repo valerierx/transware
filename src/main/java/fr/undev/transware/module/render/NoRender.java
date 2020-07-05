c8b6b1a1e84d1cd563b0baaa515a3f7fa12ccccc
undevdecatos <jeigfovhjnpien fpiokvjnebveniv>
2020-07-05 14:04:16 +0200

    renamed file, will fix completely today

--- src/main/java/fr/undev/transware/module/render/NoRender.java
+++ src/main/java/fr/undev/transware/module/render/NoRender.java
@@ -2,6 +2,27 @@ package fr.undev.transware.module.render;
 
 import fr.undev.transware.module.Module;
 
-public class NoRender extends Module {
+import net.minecraftforge.fml.common.gameevent.TickEvent;
 
+public class NoRender extends Module {
+    public NoRender(String name) {
+        super(name);
+    }
+
+    @Override
+    public void onEnable() {
+        
+
+    }
+
+    @Override
+    public void onDisable() {
+        
+
+    }
+
+    @Override
+    public void onTick(TickEvent.ClientTickEvent var1) {
+
+    }
 }
